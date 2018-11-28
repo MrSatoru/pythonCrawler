306e02f61aca1146f3a82654bf7f88ed8d3b360c
MrSatoru <dennis-hauck@web.de>
2018-11-28 22:55:22 +0100

    Auslesen das aktuellen DAX Stand mit Zeitpunkt

--- learnBSoupAndReq.py
+++ learnBSoupAndReq.py
@@ -1,10 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 req = requests.get("http://www.boerse-frankfurt.de/index/DAX")
 doc = BeautifulSoup(req.text, "html.parser")
 
-for element in doc.select("div.push-value"):
-    print(element.text)
+def getCoursValeAndTime():
+
+    value = None
+    time_stamp = None
+
+    for element in doc.select("div.push-value"):
+        if re.match('^\d+[.]\d{3}[,]\d{2}$',element.text):
+            value = element.text
+        elif re.match('^\d{2}[:]\d{2}[:]\d{2}$',element.text):
+            time_stamp = element.text
+
+    print("DAX Stand um " + time_stamp + " betrug " + value)
+
+
+getCoursValeAndTime()
+
+
+
+
+
+
 
