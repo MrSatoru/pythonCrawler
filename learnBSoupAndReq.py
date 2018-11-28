95adb13fa353b38c03c24e7e20d3d322d6d88137
MrSatoru <dennis-hauck@web.de>
2018-11-28 23:12:29 +0100

    Refactoring: Klasse Index mit den Attributen value und time_stamp

--- learnBSoupAndReq.py
+++ learnBSoupAndReq.py
@@ -3,6 +3,12 @@ import requests
 import re
 
 
+class Index:
+    def __init__(self, value, timestamp):
+        self.value = value
+        self.timestamp = timestamp
+
+
 req = requests.get("http://www.boerse-frankfurt.de/index/DAX")
 doc = BeautifulSoup(req.text, "html.parser")
 
@@ -13,15 +19,14 @@ def get_value_timestamp():
     time_stamp = None
 
     for element in doc.select("div.push-value"):
-        if re.match('^\d+[.]\d{3}[,]\d{2}$',element.text):
+        if re.match('^\d+[.]\d{3}[,]\d{2}$', element.text):
             value = element.text
-        elif re.match('^\d{2}[:]\d{2}[:]\d{2}$',element.text):
+        elif re.match('^\d{2}[:]\d{2}[:]\d{2}$', element.text):
             time_stamp = element.text
 
-    print("DAX Stand um " + time_stamp + " betrug " + value)
+    dax = Index(value, time_stamp)
 
-
-get_value_timestamp()
+    return dax
 
 
 
